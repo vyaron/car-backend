e61687e52c62a14b524337bba9ae12f1ff3cb11a
vyaron <vyaron@gmail.com>
2024-11-26 16:28:05 +0200

    config

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : process.env.DB_NAME || 'tester_db'
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://puki:puki1234@mycluster.hipy1.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster',
+  dbName : process.env.DB_NAME || 'app_db'
 }
